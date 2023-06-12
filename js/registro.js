c38becc700a400aa128288fa1b72ff5a83fd5dda
Gastón Villalba <gastonleovillalba@gmail.com>
2023-06-12 16:37:02 -0300

    ADD validación de input

--- js/registro.js
+++ js/registro.js
@@ -1,30 +1,86 @@
 "use strict"
+let first_name;
+let last_name;
+let dni;
+let birthday;
+let email;
+let password;
+let address;
+let payment;
+let payment_data;
 const form = document.getElementById("register-form");
 
 document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        //ToDo! validarDatos()
+        if (!isValidInput()) {
+            //ToDo! Mostrar mensaje dependiendo del error
+            alert("Ingrese los datos correctamente");
+            return;
+        }
+
         const new_user = createUser();
         console.log("Usuario creado:");
         console.log(new_user);
+        
         if (!existsUser(new_user)) {
             registerNewUser(new_user);
         }
     });
 });
 
-function createUser() {
-    const first_name = form.firstname.value;
-    const last_name = form.lastname.value;
-    const dni = form.dni.value;
-    const birthday = form.birthday.value;
-    const email = form.email.value;
-    const password = form.password.value;
-    const address = form.address.value;
-    const payment = form.payment.value;
-    const payment_data = readPaymentInput(payment);
+function readInput() {
+    first_name = form.firstname.value;
+    last_name = form.lastname.value;
+    dni = form.dni.value;
+    birthday = form.birthday.value;
+    email = form.email.value;
+    password = form.password.value;
+    address = form.address.value;
+    payment = form.payment.value;
+    payment_data = readPaymentInput(payment);
+}
 
+function isValidInput() {
+    readInput();
+
+    const vaild_first_name = first_name.length > 1 && /^((?![0-9.,!?:;_|+\-*\\/=%°@&#§$"'`¨^ˇ()\[\]<>{}])[\S])+$/.test(first_name);
+    const vaild_last_name = last_name.length > 1 && /^((?![0-9.,!?:;_|+\-*\\/=%°@&#§$"'`¨^ˇ()\[\]<>{}])[\S])+$/.test(last_name);
+    const valid_dni = dni.length > 7 && dni.length < 12 && /^\d+$/.test(dni);
+    const valid_birthday = '1923-16-06' < birthday && birthday < '2005-06-16';
+    const valid_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const valid_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[.,*!#$%&='¿+¡?]).{8,}$/.test(password); //8 caracteres con al menos {1 mayus, 1 minus, 1 símbolo}
+    const valid_address =/^(?:[\w+\s*]+\s*,\s*\w+\s*\w*){2}$/.test(address); //Tres oraciones separadas por dos comas 
+    const valid_payment = ['cash', 'credit', 'debit', 'mp', 'transfer'].includes(payment);
+    const valid_payment_data =  isValidPaymentData();
+
+    return vaild_first_name && vaild_last_name && valid_dni && valid_birthday && valid_email && valid_password && valid_address && valid_payment && valid_payment_data;
+}
+
+function isValidPaymentData() {
+    if (payment === 'credit' || payment === 'debit') {
+        const valid_card_name = /^(?:[a-zA-z]+ +[a-zA-Z]+)$/.test(payment_data.titular);
+        const valid_card_number = /^[0-9]{16}$/.test(payment_data.numero);
+        const valid_card_code = /^[0-9]{3}$/.test(payment_data.codigo);
+        const valid_card_expiration = /^(?:0[1-9]{1}[0-9]{2}|1[0-2]{1}[0-9]{2})$/.test(payment_data.expira);
+        return valid_card_name && valid_card_number && valid_card_code && valid_card_expiration;
+    }
+
+    if (payment === 'mp') {
+        const valid_cvu = /^[0-9]{22}$/.test(payment.cvu);
+        const valid_alias = /^(?:[a-zA-z0-9]+\.*[a-zA-z0-9]+){6,20}$/.test(payment.alias); //alfanumérico con punto entre 6 y 22 caracteres
+        return valid_cvu && valid_alias;
+    }
+
+    if (payment === 'transfer') {
+        return /^[0-9]{22}$/.test(payment.cbu);
+    }
+
+    return true;
+
+}
+
+function createUser() {
     return new User(first_name, last_name, dni, birthday, email, password, address, payment, payment_data);
 }
 
